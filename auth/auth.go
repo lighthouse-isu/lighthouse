359594f1e05b85096f54c9ce3235f66ff4d8e122
theMagicalKarp <gameboy1092@gmail.com>
2014-12-01 14:18:52 -0600

    If not logged in redirect to login page

--- auth/auth.go
+++ auth/auth.go
@@ -92,10 +92,10 @@ func AuthMiddleware(h http.Handler, ignorePaths []string) http.Handler {
 
         if loggedIn := session.GetValueOrDefault(r, "auth", "logged_in", false).(bool); loggedIn {
             h.ServeHTTP(w, r)
-        } else {
-            w.WriteHeader(http.StatusUnauthorized)
-            fmt.Fprintf(w, "unauthorized")
+            return
         }
+
+        http.Redirect(w, r, "/login",  http.StatusFound)
     })
 }
 
