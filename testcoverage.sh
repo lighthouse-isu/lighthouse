b53342a5276048c6ceb618228855667460ce5d2b
Caleb Brose <brose.caleb@gmail.com>
2015-02-23 20:48:00 -0600

    Script does each package individually

--- testcoverage.sh
+++ testcoverage.sh
@@ -2,13 +2,10 @@
 
 list="$(find -path '.*test.go')"
 
-echo "mode: count\n" > test_coverage.out
-
 for d in $list; do
 	path=$(echo $d | awk -F/ '{for (i=1; i<NF; i++) printf $i"/"}')
 
 	go test -v -coverprofile=cover.out -covermode=count $path
-	tail -n +2 cover.out >> test_coverage.out
-done
 
-$HOME/gopath/bin/goveralls -coverprofile=test_coverage.out -service=travis-ci
+	$HOME/gopath/bin/goveralls -coverprofile=cover.out -service=travis-ci
+done
