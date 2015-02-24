d78cd09983c612fd683b982be7c9894aba0f8dde
Caleb Brose <brose.caleb@gmail.com>
2015-02-23 21:20:03 -0600

    Trying a one-shot again

--- testcoverage.sh
+++ testcoverage.sh
@@ -2,14 +2,13 @@
 
 list="$(find -path '.*test.go')"
 
-echo "mode: count\n" > profile.out
+echo "mode: count\n" > profile.cov
 
 for d in $list; do
 	path=$(echo $d | awk -F/ '{for (i=1; i<NF; i++) printf $i"/"}')
 
-
 	go test $path -v -coverprofile=cover.out -covermode=count
-    tail -n +2 cover.out >> profile.out
+    tail -n +2 cover.out >> profile.cov
 done
 
 $HOME/gopath/bin/goveralls -coverprofile=profile.cov -service=travis-ci
