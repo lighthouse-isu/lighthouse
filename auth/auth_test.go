40bf020e807008ecce0d4804ac3c4cb6a6b8f7a4
Caleb Brose <brose.caleb@gmail.com>
2015-03-03 17:24:46 -0600

    Whoami unit tests

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -18,7 +18,14 @@ package auth
 import (
     "testing"
 
+    "net/http"
+    "net/http/httptest"
+
     "github.com/stretchr/testify/assert"
+
+    "github.com/gorilla/mux"
+
+    "github.com/lighthouse/lighthouse/session"
 )
 
 
@@ -26,4 +33,42 @@ func Test_SaltPassword(t *testing.T) {
     SECRET_HASH_KEY = "i'm the secret hash key"
     expectedResult := "00a987631776516d7dd00e8ace06c5c5a83739dbf95742ddf5f39eeda1f26c346f235131b4bc1a1eb244d479f899610f420e23cefb139d47c0d9a07ed1bf909c"
     assert.Equal(t, expectedResult, SaltPassword("12345", "i'm the salt"))
+}
+
+func Test_WhoAmI_NotLoggedIn(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/whoami", nil)
+
+    m := mux.NewRouter()
+    Handle(m)	
+    w := httptest.NewRecorder()
+
+    m.ServeHTTP(w, r)
+
+    assert.Equal(t, 404, w.Code)
+}
+
+func Test_WhoAmI_LoggedIn(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/whoami", nil)
+    session.SetValue(r, "auth", "email", "TEST_EMAIL")
+
+    m := mux.NewRouter()
+    Handle(m)	
+    w := httptest.NewRecorder()
+
+    m.ServeHTTP(w, r)
+
+    assert.Equal(t, 200, w.Code)
+    assert.Equal(t, "TEST_EMAIL", w.Body.String())
+}
+
+func Test_WhoAmI_NotLoggedIn(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/whoami", nil)
+
+    m := mux.NewRouter()
+    Handle(m)	
+    w := httptest.NewRecorder()
+
+    m.ServeHTTP(w, r)
+
+    assert.Equal(t, 404, w.Code)
 }
